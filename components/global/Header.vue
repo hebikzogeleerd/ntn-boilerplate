e6ef293f5e4b02d04777510cd439aa749f02b74b
hebikzogeleerd <86666392+hebikzogeleerd@users.noreply.github.com>
2021-07-02 10:48:14 +0200

    Update Header.vue

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -5,10 +5,10 @@
         <nuxt-link class="btn block" to="/">Home</nuxt-link>
       </li>
       <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
+        <nuxt-link class="btn block" to="/blog">Wat ik geleerd heb</nuxt-link>
       </li>
       <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/projects">Projects</nuxt-link>
+        <nuxt-link class="btn block" to="/projects">Voorbeelden</nuxt-link>
       </li>
     </ul>
   </nav>
